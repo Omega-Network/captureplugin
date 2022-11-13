23e106e0a24d8c39405ea1977ae03f1402e9bed0
Sam Bloom <volas@mindustry.me>
2022-11-13 00:13:23 -0500

    more efficient

--- src/main/java/en/omega/capture/Capture.java
+++ src/main/java/en/omega/capture/Capture.java
@@ -1,7 +1,10 @@
+package en.omega.capture;
+
 import arc.Events;
 import arc.math.geom.Position;
 import arc.util.Timer;
 import mindustry.Vars;
+import mindustry.content.Blocks;
 import mindustry.content.Fx;
 import mindustry.entities.Units;
 import mindustry.game.EventType;
@@ -21,36 +24,56 @@ public class Capture extends Plugin {
   
   public Capture() {
     Events.on(EventType.BlockDestroyEvent.class, e -> {
-          if (!(e.tile.build instanceof mindustry.world.blocks.storage.CoreBlock.CoreBuild) || Vars.state.gameOver)
-            return; 
-          Tile tile = e.tile;
-          Block block = tile.block();
-          Building build = tile.build;
-          Team oldTeam = build.team();
-          Unit capturer = Units.closestEnemy(oldTeam, tile.worldx(), tile.worldy(), Float.MAX_VALUE, ());
-          if (oldTeam == Team.derelict)
+        Timer.schedule(()->{
+          if (e.tile.block() == Blocks.coreShard) {
+            Building core = e.tile.build;
+            if (core != null) {
+              Team team = core.team;
+              if (team != Team.derelict) {
+                int count = 0;
+                for (Tile tile : Vars.world.tiles) {
+                  if (tile.block() == Blocks.coreShard) {
+                    Building building = tile.build;
+                    if (building != null && building.team == team) {
+                      count++;
+                    }
+                  }
+                }
+                if (count == 0) {
+                  for (Tile tile : Vars.world.tiles) {
+                    if (tile.block() == Blocks.coreFoundation) {
+                      Building building = tile.build;
+                      if (building != null && building.team == team) {
+                        building.kill();
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }, 0.1f);
+        if (!(e.tile.build instanceof mindustry.world.blocks.storage.CoreBlock.CoreBuild) || Vars.state.gameOver)
+            return;
+        Tile tile = e.tile;
+        Block block = tile.block();
+        Building build = tile.build;
+        Team oldTeam = build.team();
+        Unit capturer = Units.closestEnemy(oldTeam, tile.worldx(), tile.worldy(), Float.MAX_VALUE, u -> u.within(tile, block.size * 8.0F));
+        if (oldTeam == Team.derelict)
             for (Teams.TeamData team : Vars.state.teams.active) {
-              if (team.team == Team.derelict)
-                continue; 
-              Unit enemy = Units.closest(team.team, tile.worldx(), tile.worldy(), ());
-              if (enemy != null && (capturer == null || tile.dst((Position)enemy) < tile.dst((Position)capturer)))
-                capturer = enemy; 
-            }  
-          Team newTeam = (capturer != null) ? capturer.team : Team.derelict;
-          Call.effectReliable(Fx.upgradeCore, tile.worldx(), tile.worldy(), block.size, newTeam.color);
-          Call.infoPopup("Team [#" + newTeam.color.toString() + "]" + newTeam.name + " []captured team [#" + oldTeam.color.toString() + "]" + oldTeam.name + "[] core at " + tile.x + ", " + tile.y, 5.0F, 1, 0, 0, 50 * this.messageQueue - 50 * this.messageLimit, 0);
-          this.messageQueue = (this.messageQueue + 1) % this.messageLimit;
-          Timer.schedule((), 0.5F);
-          Timer.schedule((), 5.0F);
-          boolean teams = true;
-          for (Teams.TeamData team : Vars.state.teams.active) {
-            if (team.team != Team.malis && team.team != newTeam && (team.team != oldTeam || (oldTeam.data()).cores.size != 1) && team.hasCore()) {
-              teams = false;
-              break;
-            } 
-          } 
-          if (teams)
-            Events.fire(new EventType.GameOverEvent(newTeam)); 
-        });
-  }
+                if (team.team == Team.derelict)
+                    continue;
+                Unit enemy = Units.closest(team.team, tile.worldx(), tile.worldy(), Float.MAX_VALUE, u -> u.within(tile, block.size * 8.0F));
+                if (enemy != null && (capturer == null || tile.dst((Position) enemy) < tile.dst((Position) capturer)))
+                    capturer = enemy;
+            }
+        Team newTeam = (capturer != null) ? capturer.team : Team.derelict;
+        Call.effectReliable(Fx.upgradeCore, tile.worldx(), tile.worldy(), block.size, newTeam.color);
+        Call.infoPopup("Team [#" + newTeam.color.toString() + "]" + newTeam.name + " []captured team [#" + oldTeam.color.toString() + "]" + oldTeam.name + "[] core at " + tile.x + ", " + tile.y, 5.0F, 1, 0, 0, 50 * this.messageQueue - 50 * this.messageLimit, 0);
+        this.messageQueue = (this.messageQueue + 1) % this.messageLimit;
+          build.team(newTeam);
+    });
+    }
 }
+
