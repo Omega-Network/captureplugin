2feb34020ab45d03488bf5f9ae5817f6ff735a0a
gorodmi <gorodmibot@gmail.com>
2022-11-27 13:25:13 +0300

    bingus

--- src/main/java/en/omega/capture/Capture.java
+++ src/main/java/en/omega/capture/Capture.java
@@ -2,10 +2,12 @@ package en.omega.capture;
 
 import arc.Core;
 import arc.Events;
+import mindustry.content.Fx;
 import mindustry.game.EventType;
 import mindustry.game.Team;
 import mindustry.gen.Building;
 import mindustry.gen.Bullet;
+import mindustry.gen.Call;
 import mindustry.gen.Groups;
 import mindustry.mod.Plugin;
 import mindustry.world.blocks.storage.CoreBlock.CoreBuild;
@@ -18,16 +20,23 @@ public class Capture extends Plugin {
             Team team = bullet.team();
             Team otherTeam = build.team();
             if (!(build instanceof CoreBuild)) return;
+            build.heal();
+            build.team(team);
+            Call.effectReliable(Fx.upgradeCoreBloom, build.x(), build.y(), 0, team.color);
+            String teamName = "[#" + team.color.toString() + "]" + team.name + "[]";
+            String otherTeamName = "[#" + otherTeam.color.toString() + "]" + otherTeam.name + "[]";
+            Call.sendMessage("Team " + teamName + " captured " + otherTeamName + " core at " + build.tileX() + " " + build.tileY());
             Core.app.post(() -> {
                 if (otherTeam.active()) return;
                 Groups.build.each(b -> {
                     if (b.team() != otherTeam) return;
-                    b.team(team);
+                    b.kill();
                 });
                 Groups.unit.each(u -> {
                     if (u.team() != otherTeam) return;
-                    u.team(team);
+                    u.kill();
                 });
+                Call.sendMessage("Team " + otherTeamName + " has no cores left");
             });
         });
     }
