5e496848922154ecd8004db5ea0b2e32a965c8c2
Volas171 <volas@mindustry.me>
2023-09-24 12:07:40 -0500

    Update Capture.java

--- src/main/java/en/omega/capture/Capture.java
+++ src/main/java/en/omega/capture/Capture.java
@@ -1,19 +1,18 @@
 /*
-*          DO WHATEVER THE FUCK YOU WANT TO PUBLIC LICENSE
-*                    Version 2, September 2023  
-*
-* Copyright (C) 2023 Yeet Hosting LTD <pr@mindustry.me>
-*
-* Everyone is permitted to copy and distribute verbatim or modified
-* copies of this license document, and changing it is allowed as long
-* as the name is changed.
-*
-*            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-*   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-*
-*  
-*1. You just DO WHATEVER THE FUCK YOU WANT TO.
-*2. You don't bother us about it.
+Copyright (C) 2023  YeetHosting
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 package en.omega.capture;
 
@@ -40,6 +39,8 @@ public class Capture extends Plugin {
             String teamName = "[#" + team.color.toString() + "]" + team.name + "[]";
             String otherTeamName = "[#" + otherTeam.color.toString() + "]" + otherTeam.name + "[]";
             Call.sendMessage("Team " + teamName + " captured " + otherTeamName + " core at " + build.tileX() + " " + build.tileY());
+            //DO NOT REMOVE UNLESS YOU ARE AUTHORED BY THE AUTHOR.
+            Call.infoMessage("Copyright (C) 2023 YeetHosting This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.")
             Core.app.post(() -> {
                 if (otherTeam.active()) return;
                 Groups.build.each(b -> {
